1581f8cf301843b6c584c8e2304d73e97ef2fa79
alireza-1010111 <alirezadanaei1010111@gmail.com>
2025-06-03 15:27:21 +0330

    Start and continue the project

---
+++ src/typed.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed('#typewriter', {
+    strings: [
+      "تولید کننده دستگاه های کنترل دستی اتومبیل ویژه جانبازان و معلولین (گاز،ترمز،کلاچ)"
+    ],
+    typeSpeed: 80,
+    backSpeed: 30,
+    loop: true,
+    backDelay: 7000,
+  });
+});
