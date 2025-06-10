c06ab04b0aa1d18a01593f6dbefc8b77596f4eda
alireza-1010111 <alirezadanaei1010111@gmail.com>
2025-06-10 11:43:55 +0330

    Codes cleared and mobile menu added

--- src/typed.js
+++ src/typed.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var typed = new Typed('#typewriter', {
-    strings: [
-      "تولید کننده دستگاه های کنترل دستی اتومبیل ویژه جانبازان و معلولین (گاز،ترمز،کلاچ)"
-    ],
-    typeSpeed: 80,
-    backSpeed: 30,
-    loop: true,
-    backDelay: 7000,
-  });
+  setTimeout(function () {
+    var typed = new Typed('#typewriter', {
+      strings: [
+        "تولید کننده دستگاه های کنترل دستی اتومبیل ویژه جانبازان و معلولین (گاز،ترمز،کلاچ)"
+      ],
+      typeSpeed: 80,
+      backSpeed: 30,
+      loop: true,
+      backDelay: 7000,
+      showCursor: false
+    });
+  }, 3000);
 });
